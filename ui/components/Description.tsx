c2c663c799dd887199f453e61fb0cac77362c04d
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-12 16:44:41 +0200

    Update description

--- ui/components/Description.tsx
+++ ui/components/Description.tsx
@@ -5,5 +5,7 @@ import { Text, type TextProps } from "react-aria-components";
 import { twMerge } from "tailwind-merge";
 
 export function Description(props: Readonly<TextProps>) {
-  return <Text {...props} slot="description" className={twMerge("text-gray-600 text-sm", props.className)} />;
+  return (
+    <Text {...props} slot="description" className={twMerge("mt-1 text-muted-foreground text-sm", props.className)} />
+  );
 }
