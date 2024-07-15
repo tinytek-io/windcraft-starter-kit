9a7b2e1113c300f44d31ccf1dcd8a6b6ace47787
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-15 23:18:16 +0200

    Disable ssg

--- rspress.config.ts
+++ rspress.config.ts
@@ -5,6 +5,7 @@ import { pluginPreview } from "@rspress/plugin-preview";
 import { defineConfig } from "rspress/config";
 
 export default defineConfig({
+  ssg: false,
   globalStyles: path.join(__dirname, "styles", "index.css"),
   root: path.join(__dirname, "docs"),
   route: {
