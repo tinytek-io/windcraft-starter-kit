7c8ea62247d83fdd32b689ea4f940e96c4fea507
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-17 20:22:49 +0200

    Refinements of Button

--- ui/components/Button.tsx
+++ ui/components/Button.tsx
@@ -21,22 +21,23 @@ export interface ButtonProps extends AriaButtonProps, React.RefAttributes<HTMLBu
 
 const button = tv({
   extend: focusRing,
-  base: "inline-flex gap-1 items-center justify-center w-fit whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+  base: "inline-flex gap-2 w-fit items-center justify-center w-fit whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
   variants: {
     variant: {
       primary: "bg-primary text-primary-foreground hover:bg-primary/90 pressed:bg-primary/80",
       secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80 pressed:bg-secondary/70",
       destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90 pressed:bg-destructive/80",
-      outline: "border border-input hover:bg-accent hover:text-accent-foreground pressed:bg-accent/80",
-      ghost: "hover:bg-accent hover:text-accent-foreground pressed:bg-accent/80",
+      outline:
+        "border border-input text-accent-foreground hover:bg-accent hover:text-accent-foreground/90 pressed:bg-accent/80",
+      ghost: "hover:bg-accent text-accent-foreground hover:text-accent-foreground/90 pressed:bg-accent/80",
       link: "text-primary underline-offset-4 hover:underline pressed:text-primary/80"
     },
     size: {
-      xs: "h-6 w-6",
+      xs: "h-6 w-6 shrink-0",
       sm: "h-9 rounded-md px-3",
       md: "h-10 px-4 py-2",
       lg: "h-11 rounded-md px-8",
-      icon: "h-10 w-10"
+      icon: "h-10 w-10 shrink-0"
     },
     isDisabled: {
       true: "pointer-events-none opacity-50"
